95c6acddc38c4d95ffb72cc90c14745e28e9d2fa
Abdalr7man <Eng.Abdalrhman.Abdalmonem@gmail.com>
2025-08-02 02:31:47 +0300

    Init

---
+++ crawler/client.py
@@ -0,0 +1,6 @@
+
+# TODO: Raw HTTP request to take the response (HTML)
+# TODO: parser to parse the html and extract the urls (absolute & relative path are the same just make them one url)
+# TODO: URL normalization(the / at the end, the Capital letter, strip protocols), filtering, etc
+# TODO: Main crawling logic
+# TODO: do tests for all this modules
---
+++ crawler/parser.py
@@ -0,0 +1,5 @@
+"""
+from html.parser import HTMLParser
+I will make thing like this library but small to take the HTML Code and parse it to extract the urls
+
+"""
